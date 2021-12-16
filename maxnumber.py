9cf01d78c8e7b161c46b7d426240c9480e3eb16a
Bereket_Tilahun <91558592+bekiTil@users.noreply.github.com>
2021-12-16 23:09:02 +0300

    Create maxnumber.py

---
+++ maxnumber.py
@@ -0,0 +1,15 @@
+def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        i=0
+        j=len(nums)-1
+        operation=0
+        while j>i:
+            if nums[i]+nums[j]==k:
+                i+=1
+                j-=1
+                operation+=1
+            elif nums[i]+nums[j]>k:
+                j-=1
+            else:
+                i+=1
+        return operation 
