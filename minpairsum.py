4051a6a35b83d5179dffe059ba2cb4711203ce27
Bereket_Tilahun <91558592+bekiTil@users.noreply.github.com>
2021-12-17 00:05:16 +0300

    Create minpairsum.py

---
+++ minpairsum.py
@@ -0,0 +1,12 @@
+ def minPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        i=0
+        j=len(nums)-1
+        max=0
+        while j>i:
+            if nums[i]+ nums[j]>max:
+                max=nums[i]+ nums[j]
+            i+=1
+            j-=1
+        return max
+        
