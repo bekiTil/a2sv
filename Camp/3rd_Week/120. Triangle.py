00db47f278d99409c76ace14693bc880c0e5e566
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2022-03-10 17:03:03 +0300

    Update 120. Triangle.py

--- Camp/3rd_Week/120. Triangle.py
+++ Camp/3rd_Week/120. Triangle.py
@@ -1,3 +1,6 @@
+#top_down approach
+#time complexity O(m*n) where m is the height of the triangle where n is a variable width of the triangle
+#Space Complexity O(m*n)
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         memo={}
@@ -12,3 +15,14 @@ class Solution:
                 memo[(i,j)]=min(left,right)
                 return min(left,right)
         return dfs(0,0)
+ 
+#bottom_up approach
+#time complexity O(m*n) where m is the height of the triangle where n is a variable width of the triangle
+#Space Complexity O(m*n)
+  class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp=[[0 for _ in range(j+1)]for j in range(len(triangle)+1)]
+        for i in range(len(dp)-2,-1,-1):
+            for j in range(len(dp[i])-1,-1,-1):
+                dp[i][j]=min(triangle[i][j]+dp[i+1][j],triangle[i][j]+dp[i+1][j+1])
+        return dp[0][0]
