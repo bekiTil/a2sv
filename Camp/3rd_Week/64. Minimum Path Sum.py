4ec4b01732626dbdfd6d322482f0fdd972bde542
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2022-03-10 18:28:48 +0300

    Update 64. Minimum Path Sum.py

--- Camp/3rd_Week/64. Minimum Path Sum.py
+++ Camp/3rd_Week/64. Minimum Path Sum.py
@@ -21,4 +21,11 @@ class Solution:
         
         return path(0,0)
                     
-          
+ class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        dp=[[float("inf") for _ in range(len(grid[0])+1) ]for j in range(len(grid)+1)]
+        dp[len(grid)][len(grid[0])-1]=0
+        for i in range(len(dp)-2,-1,-1):
+            for j in range(len(dp[0])-2,-1,-1):
+                dp[i][j]=min(grid[i][j]+dp[i+1][j],grid[i][j]+dp[i][j+1])
+        return dp[0][0]
