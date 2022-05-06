c02f7c57817e6f196ead9ed7e0688a5e0328e960
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2022-05-06 17:34:57 +0300

    Easy String Question

---
+++ String/28. Implement strStr().py
@@ -0,0 +1,11 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        i=0
+        j=len(needle)-1
+        while j<len(haystack):
+            if haystack[i:j+1]==needle:
+                return i
+            else:
+                i+=1
+                j+=1
+        return -1
