7c75c8becde2435f1d99f34fbf3040ec116b2893
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2022-10-18 20:52:25 +0300

    Time: 3806 ms (37.80%), Space: 31.3 MB (21.36%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -0,0 +1,36 @@
+class Solution:
+    def countPairs(self, deliciousness: List[int]) -> int:
+        if len(deliciousness) == 1:
+            return 0
+            
+        deliciousness.sort()
+    
+    
+        count=defaultdict(int)
+        
+        for i in deliciousness:
+            count[i]+=1
+        
+        max_sum=deliciousness[-1]+deliciousness[-2]
+        max_two=1
+        
+        
+        while max_sum//2 > 0:
+            max_two*=2
+            max_sum=max_sum//2
+        ans=0 
+        for i in deliciousness:
+            check=1
+            while check<=max_two:
+                if check-i>=i:
+                    if check-i==i and count[i]>0:
+                        count[i]-=1
+                        ans=(ans+count[i])% 1000000007
+                    else:
+                        ans=(ans+count[check-i])% 1000000007
+                check*=2
+        return ans
+                        
+                    
+                
+                
