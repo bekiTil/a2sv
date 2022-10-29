a4b37feedc41148001877240e4a9bd42ff3e7f11
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2022-10-29 10:36:39 +0300

    Time: 3120 ms (13.93%), Space: 29.4 MB (52.58%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -6,12 +6,12 @@ class Solution:
         
         for i in range(1,len(nums)):
             if nums[i]==nums[i-1]:
-                if dp[i-1][0] or  (i-2>=0 and dp[i-2][0] and nums[i-2]==nums[i]):
+                if dp[i-1][0] or (i-2>=0 and dp[i-2][0] and nums[i-2]==nums[i]):
                     dp[i+1][0]=True
             elif nums[i]==nums[i-1]+1:
                 if dp[i][1]==1:
                     dp[i+1][0]=True
-                    if i-2>=0  and dp[i-1][0]:
+                    if i-2>=0 and dp[i-1][0]:
                         dp[i+1][1]+=1
                     
                 elif dp[i][1]==0 and dp[i-1][0]:
