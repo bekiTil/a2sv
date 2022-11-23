8618992ef691197ea614238c40fc58575f34466c
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2022-11-23 16:14:32 +0300

    Time: 4668 ms (40.50%), Space: 158.8 MB (27.23%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -5,22 +5,28 @@ class Solution:
             graph[value].append(index+1)
            
         
-        print(graph)
+       
         dp=[0 for _ in range(len(s))]
         def dfs(value):
-            length1=0
-            length2=0
+            length1 , length2 = 0 , 0
+            
             for val in graph[value]:
+                
                 if val not in visited:
+                    
                     visited.add(val)
                     depth=dfs(val)
                     if s[val]!=s[value]:
+                        
                         if depth>=length1:
+                            
                             length1,length2=depth,length1
                         elif depth>=length2:
+                            
                             length2=depth
            
             dp[value]=(length1+length2)+1
+            
             return max(length1,length2)+1
         visited=set()
         visited.add(0)
