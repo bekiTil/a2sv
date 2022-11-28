e825d575410e7f847b155a0d0e91ee05d4884c62
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2022-11-28 14:40:34 +0300

    Time: 304 ms (90.20%), Space: 31.2 MB (6.52%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,11 +1,15 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         exist=defaultdict(int)
+        value=1
         for i in nums:
             exist[i]+=1
-        for i in range(1,max(nums)+2):
-            if exist[i]==0:
-                return i
-        return 1
+            while exist[value]!=0:
+                value+=1
+        return value
+#         for i in range(1,max(nums)+2):
+#             if exist[i]==0:
+#                 return i
+#         return 1
         
         
