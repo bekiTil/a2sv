95417bb0e51c168a404f76c7ab88549d3e8e96df
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2022-12-01 21:29:42 +0300

    Time: 2001 ms (5.67%), Space: 20.8 MB (9.68%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,40 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        graph=defaultdict(list)
+        count=defaultdict(int)
+        back=defaultdict(list)
+        cache=defaultdict(int)
+        for i in range(len(matrix)):
+            
+            for j in range(len(matrix[0])):
+                
+                for x,y in [(-1,0),(1,0),(0,-1),(0,1)]:
+                    
+                    n=i+x
+                    m=j+y
+                    
+                    if 0<=n<len(matrix) and 0<=m<len(matrix[0]) and matrix[i][j]<matrix[n][m]:
+                        graph[(i,j)].append((n,m))
+                        count[(n,m)]+=1
+                        # back[(n,m)].append((i,j))
+        level=deque([])
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if count[(i,j)]==0:
+                    level.append((i,j))
+        # print(level)
+        maximum=0
+        while level:
+            node=level.popleft()
+            for val in graph[node]:
+                count[val]-=1
+                if count[val]==0:
+                    level.append(val)
+            for val in graph[node]:
+                cache[val]=max(cache[val],cache[node]+1)
+                maximum=max(cache[val]+1,maximum)
+        print(cache)
+        if maximum==0:
+            return 1
+        return maximum
+            
