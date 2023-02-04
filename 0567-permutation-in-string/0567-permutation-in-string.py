666fd381392105cd5d0cb64414b17d1623b8e551
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2023-02-04 17:07:34 +0300

    Time: 73 ms (78.34%), Space: 13.9 MB (94.40%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count=defaultdict(int)
+        for i in s1:
+            count[i]+=1
+        val=set(s1)
+        temp=set()
+        tempcount=defaultdict(int)
+        i=0
+        length=0
+        for index,k in enumerate(s2):
+          
+            if k not in val:
+                tempcount=defaultdict(int)
+                length=0
+                i=index+1
+                continue
+            tempcount[k]+=1
+            
+                
+           
+            while i<len(s2) and tempcount[k]>count[k]:
+                tempcount[s2[i]]-=1
+                length-=1
+                i+=1
+            if tempcount[k]<=count[k]:
+                length+=1
+            if length==len(s1):
+                return True
+            
+            
