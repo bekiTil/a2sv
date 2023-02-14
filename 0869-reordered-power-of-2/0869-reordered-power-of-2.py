5c99f5604326d31b588e7053c2898e2ffce1dc26
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2023-02-14 23:55:23 +0300

    Time: 35 ms (81.25%), Space: 13.9 MB (55.78%) - LeetHub

---
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        num = sorted(str(n))
+        for i in range(32):
+            current_num = sorted(str(2**i))
+            if num == current_num:
+                return True
+        return False
