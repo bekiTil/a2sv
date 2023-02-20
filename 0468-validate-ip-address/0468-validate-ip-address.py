ded1e5afa0336c05705cb9fdd417cdbc455d1f54
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2023-02-20 23:04:21 +0300

    Time: 28 ms (85.06%), Space: 13.8 MB (98.60%) - LeetHub

---
+++ 0468-validate-ip-address/0468-validate-ip-address.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validIPAddress(self, queryIP: str) -> str:
+        temp = queryIP.split(".")
+        print(temp)
+        value = queryIP.split(":")
+        
+        upper =['a','b','c','d','e','f']
+        lower = ['A', 'B', 'C','D' , 'E' , 'F']
+        if len(temp)== 4:
+            print(temp)
+            for val in temp:
+                if not val.isdigit() or (val[0]=="0" and len(val)>1) or int(val)>255:
+                    return "Neither"
+            return "IPv4"
+        
+            
+        elif len(value)== 8:
+            
+            for val in value:
+                if len(val)> 4 or len(val)<1:
+                    return 'Neither'
+                for char in val:
+                    if not (char.isdigit() or char in upper or char in lower ) :
+                        return 'Neither'
+            return  'IPv6'
+                
+            
+        else:
+            return "Neither"
+      
+        
