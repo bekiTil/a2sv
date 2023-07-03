8ab681db9b31c0105187f6f16ab646a456d8481c
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2023-07-03 21:29:46 +0300

    Time: 962 ms (30.29%), Space: 32 MB (12.58%) - LeetHub

---
+++ 2680-maximum-or/2680-maximum-or.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximumOr(self, nums: List[int], k: int) -> int:
+        
+        prefix = [0]
+        suffix = [0]
+        
+        for i in range(len(nums)):
+            prefix.append(prefix[-1] | nums[i])
+        for i in range(len(nums)-1,-1,-1):
+            suffix.append(suffix[-1] | nums[i])
+        prefix.append(0)
+        print(prefix)
+        suffix.append(0)
+        suffix.reverse()
+       
+        
+        
+        
+        maximum = float("-inf")
+        for i in range(len(nums)):
+            maximum =max( nums[i] * (2 ** k) | prefix[i] | suffix[i+2], maximum)
+        return maximum
+            
+                
