60535cd40643296760543cf18c0998e73a443ff4
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2023-10-07 21:45:29 +0300

    Time: 62 ms (72.63%), Space: 16.3 MB (80.83%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,40 +5,21 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        newNode=ListNode(0)
-        temporary=newNode
-        add=0
-        while l1 and l2:
-            value=l1.val+l2.val+add
-            add=0
-            if value>=10:
-                value-=10
-                add=1
-                
-            newNode.next=ListNode(value)
-            newNode=newNode.next
-            l1=l1.next
-            l2=l2.next
-        # print(temporary)
-        while l1:
-            value=l1.val+add
-            add=0
-            if value>=10:
-                value-=10
-                add=1
-            newNode.next=ListNode(value)
-            newNode=newNode.next
-            l1=l1.next
-        while l2:
-            value=l2.val+add
-            add=0
-            if value>=10:
-                value-=10
-                add=1
-            newNode.next=ListNode(value)
-            newNode=newNode.next
-            l2=l2.next
-        if add!=0:
-            newNode.next=ListNode(1)
+        dummy = ListNode(0)
+        ans = dummy
+        reminder  = 0
+        while l1 or l2:
+            temp = 0
+            if l1:
+                temp+= l1.val
+                l1= l1.next
+            if l2:
+                temp += l2.val
+                l2 = l2.next
+            dummy.next= ListNode((temp+reminder)%10)
+            dummy= dummy.next
+            reminder = (temp+reminder)//10
+        if reminder>0:
+            dummy.next = ListNode(reminder)
+        return ans.next
             
-        return temporary.next
