738b2c58b7aeb4979e271af126ae9ed3ac7a1c8f
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2023-10-07 11:50:33 +0300

    Time: 93 ms (63.01%), Space: 21 MB (65.16%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,17 +1,35 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
         arr=[]
-        for i in s:
-            temp=False
-            while arr and arr[-1][0]==i and (temp or arr[-1][-1]>=k-1):
-                temp=True
-                arr.pop()
-            if not temp:
-                if arr and arr[-1][0]==i:
-                    arr.append((i,arr[-1][-1]+1))
-                else:
-                    arr.append((i,1))
-        return "".join(char for char,num in arr)
+        
+        stack = []
+        
+        for char in s:
+            
+           
+            
+            if stack and stack[-1][0] == char:
+                stack[-1][-1]+=1
+            else:
+                stack.append([char,1])
+            
+            
+            if stack[-1][-1] == k:
+                stack.pop()
+        print(stack)
+        return "".join(char*num for char,num in stack)
+                
+        # for i in s:
+        #     temp=False
+        #     while arr and arr[-1][0]==i and (temp or arr[-1][-1]>=k-1):
+        #         temp=True
+        #         arr.pop()
+        #     if not temp:
+        #         if arr and arr[-1][0]==i:
+        #             arr.append((i,arr[-1][-1]+1))
+        #         else:
+        #             arr.append((i,1))
+        # return "".join(char for char,num in arr)
                     
             
                 
